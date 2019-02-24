3050013a9bba14e78952e0972fa95077c55275d5
Russell <puhlmann8@yahoo.com>
2019-02-24 13:42:46 -0700

    Added Javadoc comments

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,34 +1,74 @@
 
 package cse360assign2;
 
+/**
+ * A basic calculator program that adds (+), subtracts (-),
+ * multiplies (*), and divides (/).
+ * 
+ * @author Russell Puhlmann
+ *
+ */
 public class Calculator {
 
 	private int total;
 	
+	/**
+	 * Constructor which initializes the calculator object.
+	 */
 	public Calculator () {
-		total = 0;  // not needed - included for clarity 
+		total = 0;  // not needed - included for clarity
 	}
 	
+	/**
+	 * Gets the current total value.
+	 * 
+	 * @return The total is the result of the calculator.
+	 */
 	public int getTotal () {
 		return 0;
 	}
 	
+	/**
+	 * Add the value to the total.
+	 * 
+	 * @param value The integer to be added to total.
+	 */
 	public void add (int value) {
 		
 	}
 	
+	/**
+	 * Subtract the value from the total.
+	 * 
+	 * @param value The integer to be subtracted from the total.
+	 */
 	public void subtract (int value) {
 		
 	}
 	
+	/**
+	 * Multiply the value to total, or add total to it's self value times.
+	 * 
+	 * @param value The number of time you are multiplying total
+	 */
 	public void multiply (int value) {
 		
 	}
 	
+	/**
+	 * Divide the total by the value.
+	 * 
+	 * @param value The denominator, can't be zero ( 0 ).
+	 */
 	public void divide (int value) {
 		
 	}
 	
+	/**
+	 * Gets the history of what the calculator has done.
+	 * 
+	 * @return A list of the history.
+	 */
 	public String getHistory () {
 		return "";
 	}
